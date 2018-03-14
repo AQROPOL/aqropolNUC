d8f83876ad7ff3c42c209d461bbc16d69c625f9d
vinyard <antoineleval@gmail.com>
2018-03-15 00:41:28 +0100

    producer

---
+++ aqropolmomConsumer/src/main/java/istic/project/aqropol/mom_consumer/MomConsumerApplication.java
@@ -0,0 +1,12 @@
+package istic.project.aqropol.mom_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomConsumerApplication.class, args);
+	}
+}
---
+++ aqropolmomConsumer/src/test/java/istic/project/aqropol/mom_consumer/MomConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package istic.project.aqropol.mom_consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MomConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
